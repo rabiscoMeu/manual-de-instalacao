d51ca321f341ab294306d43da97cb3ebc8695153
rabisco meu <95883559+rabiscoMeu@users.noreply.github.com>
2022-10-03 18:32:59 -0300

    Update main.js
    fixed manual loading error in fireFox and Safari browser

--- scripts/main.js
+++ scripts/main.js
@@ -4,39 +4,44 @@
 * generates the html through template strings.
 */
 
-const { default: productManual } = await import("../data/productsManual.json", { assert: { type: "json" } })
-
 function fillTheManualList() {
 
    let manual = document.querySelector('.manual');
-   
-   productManual.map((el, index) => {
-      const manualItens = {
-          title: el.title,
-          image: el.image,
-          alt: el.description,
-          file: el.manual_file_path,
-          anchor: el.html_url,
-      }
-      
-     const manualContainer = `
-         <li id="${manualItens.anchor}" key="${index}">
-            <h2>${manualItens.title}</h2>
-            <img src="${manualItens.image}" alt="${manualItens.alt}">
-            <a 
-             class="btn" 
-             role="button" 
-             href="${manualItens.file}" 
-             download"${manualItens.title}" 
-             target="_blank"
-             title="baixar - ${manualItens.title}" 
-            >
-               <i class="fas fa-file-download"></i>
-               download
-            </a>
+
+   const dataObject = 'https://rabiscomeu.github.io/manual-de-instalacao/data/productsManual.json';
+
+   fetch(dataObject)
+      .then(response => response.json())
+      .then(data => {
+
+         data.map((el, index) => {
+            const manualItens = {
+               title: el.title,
+               image: el.image,
+               alt: el.description,
+               file: el.manual_file_path,
+               anchor: el.html_url,
+            }
+
+            const manualContainer = `
+            <li id="${manualItens.anchor}" key="${index}">
+               <h2>${manualItens.title}</h2>
+               <img src="${manualItens.image}" alt="${manualItens.alt}">
+               <a 
+                 class="btn" 
+                 role="button" 
+                 href="${manualItens.file}" 
+                 download"${manualItens.title}" 
+                 target="_blank"
+                 title="baixar - ${manualItens.title}" 
+               >
+                  <i class="fas fa-file-download"></i>
+                  download
+               </a>
          </li>
       `
-      manual.innerHTML += manualContainer;
-   })
+            manual.innerHTML += manualContainer;
+         })
+      });
 
 } fillTheManualList();
