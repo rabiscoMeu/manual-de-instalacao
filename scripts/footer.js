697d4cc4ab21c2c6ea96f0d2c74b447b445e7cc4
rabisco meu <95883559+rabiscoMeu@users.noreply.github.com>
2022-10-03 18:34:10 -0300

    Update footer.js
    fixed manual loading error in fireFox and Safari browser

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,36 +1,39 @@
 
- /*
- * fill the social media list
- * generates the html through template strings.
- */
+/*
+* fill the social media list
+* generates the html through template strings.
+*/
 
- const { default: socialData } = await import("../data/socialData.json", { assert: { type: "json" } })
- 
- 
 function addSocialMedia() {
-    const socialMedia = document.querySelector('.social-media');
- 
-    socialData.map((el , index ) => {
-      const profile = {
-         title:  el.title,
-         url: el.url_profiles,
-         icon: el.icons,
-      }  
 
-      const socialMediaContainer = `
-         <li class="${profile.title}" key="${index}">
-            <a 
-              href="${profile.url}"
-              title="${profile.title}"
-              rel="external"
-            >
-              <i class="${profile.icon}"></i>
-            </a>
-         </li>
-      `
-      socialMedia.innerHTML += socialMediaContainer;
-})
-  
+   const socialMedia = document.querySelector('.social-media');
+
+   const dataObject = 'https://rabiscomeu.github.io/manual-de-instalacao/data/socialData.json';
+
+   fetch(dataObject)
+      .then(response => response.json())
+      .then(data => {
+         
+         data.map((el, index) => {
+            const profile = {
+               title: el.title,
+               url: el.url_profiles,
+               icon: el.icons,
+            }
+
+            const socialMediaContainer = `
+            <li class="${profile.title}" key="${index}">
+               <a 
+                 href="${profile.url}"
+                 title="${profile.title}"
+                 rel="external"
+               >
+                 <i class="${profile.icon}"></i>
+               </a>
+            </li>
+         `
+            socialMedia.innerHTML += socialMediaContainer;
+         })
+      })
+
 } addSocialMedia();
- 
- 
