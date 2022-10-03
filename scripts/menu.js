3be31dc5e2f6b3073580b2e4e56ed86dafa36d7b
rabisco meu <95883559+rabiscoMeu@users.noreply.github.com>
2022-10-03 18:33:24 -0300

    Update menu.js
    fixed manual loading error in fireFox and Safari browser

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,42 +1,43 @@
-const { default: productManual } = await import("../data/productsManual.json", { assert: { type: "json" } })
-
-
-
 /*
 * fill the menu list 
 * generates the html through template string. 
 */
 
-const menuProductsItens = productManual;
-
 function addFeaturesMenuItems() {
    const menu = document.querySelector('.menu-list');
 
-   productManual.map((el, index) => {
-      const menuItens = {
-         icon: el.icon_src,
-         title: el.icon_title,
-         alt: el.description,
-         anchor: el.html_url,
-      }
-
-      const menuContainer = `
-         <li>
-            <img 
-              src="${menuItens.icon}" 
-              alt="${menuItens.alt}" 
-              key="${index}"
-            >
-            <a 
-              title="${menuItens.title}" 
-              href="#${menuItens.anchor}"
-            > 
-               ${menuItens.title} 
-            </a>
-         </li>
-      `
-      menu.innerHTML += menuContainer;
+   const dataObject = 'https://rabiscomeu.github.io/manual-de-instalacao/data/productsManual.json';
+  
+   fetch(dataObject)
+   .then(response => response.json())
+   .then(data => {
+      data.map((el, index) => {
+         const menuItens = {
+            icon: el.icon_src,
+            title: el.icon_title,
+            alt: el.description,
+            anchor: el.html_url,
+         }
+   
+         const menuContainer = `
+            <li>
+               <img 
+                 src="${menuItens.icon}" 
+                 alt="${menuItens.alt}" 
+                 key="${index}"
+               >
+               <a 
+                 title="${menuItens.title}" 
+                 href="#${menuItens.anchor}"
+               > 
+                  ${menuItens.title} 
+               </a>
+            </li>
+         `
+         menu.innerHTML += menuContainer;
+      })
    })
+   
 
 } addFeaturesMenuItems()
 
